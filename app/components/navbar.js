023cf3ab4a94c4c6f6cfc6554544edbec3c54a59
ayshadairifallaha <aysha.dairi_fallaha05@ed.cna.nl.ca>
2026-06-03 11:56:05 -0230

    added navigation links to Navbar (Home, Listings, Recommendations, About)

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav>
-      <p>Navbar</p>
+      <div>
+        <Link href="/">Smart Apartment Finder</Link>
+        <div>
+          <Link href="/">Home</Link>
+          <Link href="/listings">Listings</Link>
+          <Link href="/recommendations">Recommendations</Link>
+          <Link href="/about">About</Link>
+        </div>
+      </div>
     </nav>
   );
 }
