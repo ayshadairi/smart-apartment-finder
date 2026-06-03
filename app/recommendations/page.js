46d21764b335dfb4970b20df8fec5a6b5b2ceecb
ayshadairifallaha <aysha.dairi_fallaha05@ed.cna.nl.ca>
2026-06-03 12:04:21 -0230

    added content and image to Home page

---
+++ app/recommendations/page.js
@@ -0,0 +1,8 @@
+export default function Recommendations() {
+  return (
+    <div>
+      <h1>AI Recommendations</h1>
+      <p>Your personalized apartment matches will appear here.</p>
+    </div>
+  );
+}
