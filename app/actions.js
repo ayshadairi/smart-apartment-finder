4136351112033213e881c9a0e0d71a5dc9a667a2
Aysha Dairi <ayshadairi@gmail.com>
2026-06-19 20:24:25 -0230

    add addApartment server action

---
+++ app/actions.js
@@ -0,0 +1,22 @@
+"use server";
+import { connectToDB } from "@/app/api/db";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export async function addApartment(formData) {
+    const { db } = await connectToDB();
+    
+    const newApartment = {
+        title: formData.get("title"),
+        price: formData.get("price"),
+        location: formData.get("location"),
+        bedrooms: parseInt(formData.get("bedrooms")) || 0,
+        description: formData.get("description"),
+        createdAt: new Date()
+    };
+    
+    await db.collection("apartments").insertOne(newApartment);
+    
+    revalidatePath("/listings");
+    redirect("/listings");
+}
